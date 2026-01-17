38eb6563000bd6db262ae7df6819765d92047962
RailyaFarakhova <railya.farakhova@ht-la.org>
2026-01-16 19:02:57 -0800

    added LimelightHelpers from docs + set up vision basics

---
+++ src/main/java/frc/robot/Constants/VisionConstants.java
@@ -0,0 +1,18 @@
+package frc.robot.Constants;
+
+public class VisionConstants {
+    public static final int limelightID = 0;
+
+    public static final double horizontalOffset = 1;
+    public static final double verticalOffset = 2; 
+    public static final double altitude = 8;
+    public static final double lensHeight = 7;
+    public static final double mountedDegree = 0;
+
+    public static double tx;
+    public static double ty; 
+    public static boolean tv; 
+
+    public static final double hubApriltagHeight = 44.25;
+    
+}
