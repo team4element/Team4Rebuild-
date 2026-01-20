5085ebfaa614f19275f1737b503c4e23d70f8574
RailyaFarakhova <railya.farakhova@ht-la.org>
2026-01-19 18:12:16 -0800

    made commands for manual climb movement

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -22,10 +22,13 @@ public class Climb extends SubsystemBase{
     private double highLimit;
     private double lowLimit;
 
+    // Used to control speed of the motor
     private DutyCycleOut m_dutyCycle;
+    // Used to control position of the motor
     private PositionVoltage m_positionRequest;
     private CurrentLimitsConfigs m_currentLimit;
 
+    // Controls print statement output to terminal
     private boolean debug;
 
     public Climb(){
@@ -37,40 +40,51 @@ public class Climb extends SubsystemBase{
         m_currentLimit.StatorCurrentLimit = ClimbConstants.currentLimit;
         m_currentLimit.StatorCurrentLimitEnable = true;
 
-        debug = false;
+        debug = false; // This should be true when testing
     }
 
+    // Constantly gets the motor's rotation while testing.
     public void periodic(){
         if(debug == true){
-        double currentPosition = m_motor.getPosition().getValueAsDouble();
-        System.out.println("CLIMB POSITION: " + currentPosition); 
-        
+        double currentRotation = m_motor.getPosition().getValueAsDouble();
+        System.out.println("CLIMB POSITION: " + currentRotation); 
+
         }
     }
 
+    /*
+     * Initiates motor starting position.
+     */
     public void resetMotor(){
         m_motor.setPosition(0);
     }
 
+    /*
+     * Stops the motor and keeps the climb at current position.
+     */
     public void hold(){
         m_motor.setControl(m_dutyCycle.withOutput(0));
         m_motor.setNeutralMode(NeutralModeValue.Brake);
     }
 
-    public void manual(double speedPercentage){
-        m_motor.setControl(m_dutyCycle.withOutput(speedPercentage));
-    }
-
-    public void pullDown(double speedPercentage){
+    /**
+     * Runs the motor in reverse to pull the robot up until it reaches the hook's physical limit.
+     * @param speedPercentage from -1 to 1.
+     */
+    public void runMotorDown(double speedPercentage){
         if(m_motor.getPosition().getValueAsDouble() == lowLimit){
             hold();
 
         } else{
-            m_motor.setControl(m_dutyCycle.withOutput(speedPercentage));
+            m_motor.setControl(m_dutyCycle.withOutput(-speedPercentage));
         }
     }
 
-    public void climbUp(double speedPercentage){
+    /**
+     * Runs the motor to reach to the next level until it reaches the hook's physical limit.
+     * @param speedPercentage from -1 to 1.
+     */
+    public void runMotorUp(double speedPercentage){
         if(m_motor.getPosition().getValueAsDouble() == highLimit){
             hold();
 
@@ -79,6 +93,10 @@ public class Climb extends SubsystemBase{
         }
     }
 
+    /**
+     * Assigns a goal value for the climb position to reach next level. 
+     * @param setpoint based on motor rotation.
+     */
     public void goToSetpoint(double setpoint){
         m_motor.setControl(m_positionRequest.withPosition(setpoint));
     }
