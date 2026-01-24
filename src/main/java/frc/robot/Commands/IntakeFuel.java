cb8b3c76762707f6a20831aa117800a0f4eb6a27
RailyaFarakhova <railya.farakhova@ht-la.org>
2026-01-24 11:38:06 -0800

    removed uncessary comments

--- src/main/java/frc/robot/Commands/IntakeFuel.java
+++ src/main/java/frc/robot/Commands/IntakeFuel.java
@@ -1,13 +1,8 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.Commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Subsystems.Intake;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class IntakeFuel extends Command {
   /** Creates a new Intake. */
   public Intake m_intake;
